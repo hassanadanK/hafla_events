c92cd6813914dd71ddcf431de835bb3450f86c99
hassanadanK <hassanabdullahibukura@gmail.com>
2024-06-08 22:34:48 +0300

    Updated some variables

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,7 +1,7 @@
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
